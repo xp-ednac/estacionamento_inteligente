10e1e10764c63ea26837f35e224976b226ffd441
thalesgmendes <thalesmendes551@gmail.com>
2024-06-17 10:05:12 -0300

    atualizando mqtt

--- sketch_mqtt-sensor.c
+++ sketch_mqtt-sensor.c
@@ -1,109 +1,82 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include "DHT.h"
 
-#define pin_a D0 
-#define pin_b D1
-#define pin_c D2 
-#define pin_d D3 
-#define pin_e D4 
-#define TOPIC_PUBLISH "RECEBE_DADOS_ESP" // Tópico 
+#define DHT_PIN D7
+#define DHT_TYPE DHT11
 
-char buf[30];
-int val_a, val_b, val_c, val_d, val_e; // Declarando as variáveis val no escopo global.
-int lastAState, lastBState, lastCState, lastDState, lastEState; // Variável para armazenar o estado anterior de cada pino.
+DHT dht11(DHT_PIN, DHT_TYPE);
 
-const char *ssid = "Android";  // ssid
-const char *password = "1234567890";  // Substitua pela sua senha
-const char *mqtt_broker = "mqtt.eclipseprojects.io"; 
-const int mqtt_port = 1883;  // Substitua pela porta correta do broker MQTT
-const char *topic = "esp8266/inputs";
+
+const char *ssid = "thales";
+const char *password = "123456";
+
+const char *mqtt_broker = "broker.hivemq.com";
+const char *temperature_topic = "room/temperature";
+const char *humidity_topic = "room/humidity";
+const int mqtt_port = 1883;
+
+char msg[50];
+
+bool mqttStatus = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Declaração da função getLeitura
-void getLeitura();
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(pin_a, INPUT);
-    pinMode(pin_b, INPUT);
-    pinMode(pin_c, INPUT);
-    pinMode(pin_d, INPUT);
-    pinMode(pin_e, INPUT);
-
-    // Conecta-se à rede Wi-Fi
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.println("Conectando ao WiFi...");
-    }
-    Serial.println("Conectado à rede Wi-Fi");
-
-    // Configura o servidor MQTT
-    client.setServer(mqtt_broker, mqtt_port);
-    client.setCallback(callback);
-
-    // Conecta-se ao servidor MQTT
-    reconnect();
-}
-
-void loop() {
-    // Mantém a conexão MQTT ativa
-    if (!client.connected()) {
-        reconnect();
-    }
-
-    // Realiza a leitura de cada pino
-    val_a = digitalRead(pin_a);
-    val_b = digitalRead(pin_b);
-    val_c = digitalRead(pin_c);
-    val_d = !digitalRead(pin_d);
-    val_e = !digitalRead(pin_e);
-
-    // Verifica mudanças em cada pino e publica no broker se necessário
-    if (val_a != lastAState) {
-        getLeitura('a', val_a);
-        lastAState = val_a;
-    }
-
-    if (val_b != lastBState) {
-        getLeitura('b', val_b);
-        lastBState = val_b;
-    }
-
-    if (val_c != lastCState) {
-        getLeitura('c', val_c);
-        lastCState = val_c;
-    }
-
-    if (val_d != lastDState) {
-        getLeitura('d', val_d);
-        lastDState = val_d;
-    }
-
-    if (val_e != lastEState) {
-        getLeitura('e', val_e);
-        lastEState = val_e;
-    }
-
-    // Aguarda antes de enviar novas leituras
-    delay(1000);
+  Serial.begin(9600);
   
-    // Mantém a conexão MQTT ativa
-    client.loop();
+  dht11.begin();
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Conectando ao WiFi...");
+  }
+  Serial.println("");
+  Serial.println("Conectado à rede Wi-Fi");
+
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+
+  reconnect();
+
 }
 
-// Função que realiza a leitura do sensor e enviar para o tópico Publisher
-void getLeitura(char pino, int valor) {
-    dtostrf(valor, 6, 2, buf);
-    String topic_pino = "esp8266/inputs/" + String(pino);
-    client.publish(topic_pino.c_str(), buf);
-    Serial.print("Pino ");
-    Serial.print(pino);
-    Serial.print(": ");
-    Serial.println(buf);
-    Serial.println("Payload enviado!");
+void loop(){
+
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  float temperature_C = dht11.readTemperature();
+  float humi = dht11.readHumidity();
+
+  if ( isnan(temperature_C) || isnan(humi)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+    sprintf(msg, "%.2f", humi);
+    client.publish(humidity_topic, msg);
+
+    Serial.print("  |  ");
+
+    Serial.print("Temperature: ");
+    Serial.print(temperature_C);
+    Serial.print("°C  ~  ");
+    sprintf(msg, "%.2f", temperature_C);
+    client.publish(temperature_topic, msg);
+  }
+
+  delay(2000);
+
+  client.loop();
+
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
@@ -124,7 +97,8 @@ void reconnect() {
         Serial.printf("Conectando o cliente %s ao broker MQTT\n", client_id.c_str());
         if (client.connect(client_id.c_str())) {
             Serial.println("Conectado ao broker MQTT!");
-            client.subscribe(topic);
+            client.subscribe(temperature_topic);
+            client.subscribe(humidity_topic);
         } else {
             Serial.print("Falha na conexão: ");
             Serial.print(client.state());
@@ -132,3 +106,4 @@ void reconnect() {
         }
     }
 }
+
